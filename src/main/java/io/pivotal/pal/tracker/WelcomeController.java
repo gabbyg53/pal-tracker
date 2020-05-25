d62dfd053adacb37388b21228263dcfede631522
gabbyg53 <ggriggs@g.clemson.edu>
2020-05-24 19:04:06 -0500

    finished lab3

--- src/main/java/io/pivotal/pal/tracker/WelcomeController.java
+++ src/main/java/io/pivotal/pal/tracker/WelcomeController.java
@@ -1,12 +1,19 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
+
     @GetMapping("/")
     public String sayHello() {
-        return "hello";
+        return msg;
+    }
+    private String msg;
+
+    public WelcomeController(@Value("${welcome.message}") String msg) {
+        this.msg = msg;
     }
 }
